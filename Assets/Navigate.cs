d79bd343f297bbf9e7a3220ea3ee637f474ddfa9
Paul Sinnett <paul.sinnett@gmail.com>
2017-11-15 13:23:40 +0000

    Initial commit

---
+++ Assets/Navigate.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Navigate : MonoBehaviour
+{
+    public Transform start;
+    public Transform end;
+    NavMeshAgent agent;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        StartCoroutine(Patrol());
+    }
+
+    IEnumerator Patrol()
+    {
+        while (true)
+        {
+            agent.SetDestination(start.position);
+            yield return new WaitUntil(() => Vector3.Distance(transform.position, start.position) < 0.2f);
+            agent.SetDestination(end.position);
+            yield return new WaitUntil(() => Vector3.Distance(transform.position, end.position) < 0.2f);
+        }
+    }
+}
---
+++ Assets/SlowTime.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SlowTime : MonoBehaviour {
+
+    public Slider slider;
+    float timeScale = 0.0f;
+
+    void Start()
+    {
+        slider.value = Time.timeScale;
+    }
+
+    public void ChangeTime(float time)
+    {
+        Time.timeScale = time;
+    }
+
+    public void Pause()
+    {
+        float tempScale = Time.timeScale;
+        Time.timeScale = timeScale;
+        timeScale = tempScale;
+    }
+}
